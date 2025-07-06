8a6cb5a62d6db1aa4d3d7c23cbde20fd050b845c
Saman-Safaei-Dev <xsamansafaeix2@gmail.com>
2025-07-07 02:46:49 +0330

    feat: add react query to the project with example request for axios

---
+++ app/components/QueryProvider.tsx
@@ -0,0 +1,16 @@
+import type { ReactNode } from "react";
+import { QueryClientProvider } from "@tanstack/react-query";
+
+import queryClient from "~/lib/react-query";
+
+type Props = { children?: ReactNode };
+
+function QueryProvider(props: Props) {
+  const { children } = props;
+
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+}
+
+export default QueryProvider;
