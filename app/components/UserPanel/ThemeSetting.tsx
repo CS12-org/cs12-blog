1587ad1528e60480dc777e4408448663ab4d0b6c
Fatima.Baghandeh <fatima.baghandeh@gmail.com>
2025-07-10 18:31:57 +0330

    feat: (wip) user panel page

---
+++ app/components/UserPanel/ThemeSetting.tsx
@@ -0,0 +1,19 @@
+import Button from "~/components/Button"
+import twMerge from "~/lib/tw-merge";
+
+type ThemeSettingProps = {
+  buttonText: string;
+  bgColorClass: string; 
+};
+
+function ThemeSetting({ buttonText, bgColorClass }: ThemeSettingProps) {
+  return (
+    <div className="flex bg-mantle  rounded-md">
+      <div className={twMerge("", bgClassName)}> </div>
+    {/* <div className={`${bgColorClass} h-3 w-3 m-2.5 rounded-bl rounded-tr`}></div> */}
+      <Button className="m-2.5 text-xs ">{buttonText} </Button>
+    </div>
+  );
+}
+
+export default ThemeSetting;
--- app/routes.ts
+++ app/routes.ts
@@ -1,10 +1,11 @@
 import { type RouteConfig, layout, route } from "@react-router/dev/routes";
 
 export default [
-  layout("layout/Main.tsx", [route("/", "routes/Home.tsx")]),
+  layout("layout/Main.tsx", [ route("/", "routes/UserPanel.tsx")]),
 
   route("/login", "routes/Login.tsx"),
   route("/signup", "routes/SignUp.tsx"),
 
   route("/api/theme", "routes/api/theme.ts"),
+ 
 ] satisfies RouteConfig;
