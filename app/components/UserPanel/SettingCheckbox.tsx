46be6ca1e71e9fd17a86abdc15a35eef3b5e222b
fatimanotron <fatimanotron@gmail.com>
2025-07-15 23:18:09 +0330

    WIP : added sidebar, used react-aria components, styled logo

--- app/components/UserPanel/SettingCheckbox.tsx
+++ app/components/UserPanel/SettingCheckbox.tsx
@@ -11,20 +11,20 @@ import {
   Text,
 } from "react-aria-components";
 
-interface MyCheckboxGroupProps extends Omit<CheckboxGroupProps, "children"> {
+interface SettingCheckboxGroupProps extends Omit<CheckboxGroupProps, "children"> {
   children?: React.ReactNode;
   label?: string;
   description?: string;
   errorMessage?: string | ((validation: ValidationResult) => string);
 }
 
-export function MyCheckboxGroup({
+export function SettingCheckboxGroup({
   label,
   description,
   errorMessage,
   children,
   ...props
-}: MyCheckboxGroupProps) {
+}: SettingCheckboxGroupProps) {
   return (
     <CheckboxGroup {...props}>
       {label && <Label>{label}</Label>}
@@ -35,33 +35,34 @@ export function MyCheckboxGroup({
   );
 }
 
-interface MyCheckboxProps extends Omit<CheckboxProps, "children"> {
+interface SettingCheckboxOptionProps extends Omit<CheckboxProps, "children"> {
   children?: React.ReactNode;
 }
 
-function MyCheckbox({ children, ...props }: MyCheckboxProps) {
+export function SettingCheckboxOption({
+  children,
+  ...props
+}: SettingCheckboxOptionProps) {
   return (
     <Checkbox {...props}>
       {({ isSelected }) => (
-        <div className="flex items-center justify-between gap-2.5 w-full  ">
-          {/* باکس کلیک‌پذیر */}
+        <div className="flex items-center justify-between gap-2.5 w-full">
           <div
-            className={`flex-1 h-9 rounded-md px-3 py-2 text-xs text-subtext-1 cursor-pointer flex items-center  ${
-              isSelected ? "bg-mantle text-maroon" : "bg-mantle "
+            className={`flex-1 h-9 rounded-md px-3 py-2 text-xs text-subtext-1 cursor-pointer flex items-center ${
+              isSelected ? "bg-mantle text-maroon" : "bg-mantle"
             }`}
           >
             {children}
           </div>
 
-          {/* دایره مستقل، ولی وابسته به isSelected */}
           <div
-            className="relative h-9 w-9 shrink-0 flex items-center justify-center "
+            className="relative h-9 w-9 shrink-0 flex items-center justify-center"
             aria-hidden="true"
           >
-            <div className="absolute h-9 w-9 rounded-md bg-base opacity-40 " />
+            <div className="absolute h-9 w-9 rounded-md bg-base" />
             <div
               className={`relative h-4 w-4 rounded-full transition-colors duration-300 cursor-pointer ${
-                isSelected ? "bg-teal" : "bg-crust"
+                isSelected ? "bg-rosewater shadow-[inset_5px_5px_5.8px_#ed8796]" : "bg-crust"
               }`}
             />
           </div>
@@ -70,6 +71,3 @@ function MyCheckbox({ children, ...props }: MyCheckboxProps) {
     </Checkbox>
   );
 }
-
-
-export default  MyCheckbox;
--- app/routes/UserPanel.tsx
+++ app/routes/UserPanel.tsx
@@ -2,40 +2,51 @@ import UserPanelList from "~/components/UserPanel/UserPanelList";
 import Profile from "../assets/images/user-profile.png";
 import PlusSign from "~/assets/images/plus-sign.svg?react";
 import ThemeSetting from "~/components/UserPanel/ThemeSetting";
-import MyCheckbox, {
-  MyCheckboxGroup,
+import {
+  SettingCheckboxGroup,
+  SettingCheckboxOption,
 } from "~/components/UserPanel/SettingCheckbox";
 import MyRadioGroup from "~/components/UserPanel/ThemeSetting";
 import { Radio } from "react-aria-components";
-import { MyTextField } from "~/components/UserPanel/TextFeild";
+import { TextInput} from "~/components/UserPanel/TextFeild";
 import Button from "~/components/Button";
-import Cs12Logo from "~/components/Global/Icons/cs12-logo";
+import Cs12Logo from "../components/Global/Icons/cs12-logo.svg?react";
+import {Text} from "react-aria-components"
+import { useState } from "react";
+
 
 function UserPanel() {
+    const [mobileSidebarOpen, setMobileSidebarOpen] = useState(false);
+  
   return (
-    <div className="flex gap-x-5 w-full ">
-      <UserPanelList />
-      <main className="flex flex-col gap-4 w-full text-subtext-1">
+    <div className="flex gap-x-5 w-full items-start">
+      
+      <UserPanelList
+            mobileSidebarOpen={mobileSidebarOpen}
+        onMobileSidebarChange={setMobileSidebarOpen} />
+      <main className="flex flex-col gap-2.5 w-full text-subtext-1">
         <div className="flex justify-between items-center">
           <h1 className="font-extrabold text-xl">اطلاعات اولیه پروفایل</h1>
           <Button className="bg-sapphire text-sm h-[36px] w-[39px] text-crust rounded">
             ثبت
           </Button>
         </div>
-        <div className="bg-crust p-2 rounded-xl flex flex-col w-full  ">
+        <div className="bg-crust  rounded-xl flex flex-col w-full  ">
           <div className="grid grid-cols-6 grid-rows-1 content-center items-center ">
-            <Cs12Logo />
+            <div className=" rounded-tr-xl   bg-mantle w-29.5 h-full flex items-center rounded-bl-[100px] shadow-[_-1px_3px_5px_rgba(0,0,0,0.3)]">
+            <Cs12Logo className="mr-1.5"/>
+            </div>
             <img
               src={Profile}
-              className="w-18 h-18 row-start-1 mr-[20px] justify-self-start rounded-tr-[40px] rounded-bl-[40px] rounded-tl-3xl rounded-br-3xl"
+              className="w-20 h-20 row-start-1 col-start-2 col-end-3 mr-[25px] justify-self-start rounded-tr-[40px] rounded-bl-[40px] rounded-tl-3xl rounded-br-3xl"
             />
 
-            <div className="col-start-3 col-end-7 flex flex-col gap-y-2">
+            <div className="col-start-3 col-end-7 flex flex-col gap-y-2 m-2.5">
               <h1 className="p-2 w-full rounded-md content-center bg-mantle h-13 text-xl font-extrabold">
                 سارا امیری
               </h1>
               <form className="w-full">
-                <MyTextField
+                <TextInput
                   name="توضیح کوتاه از فرد"
                   placeholder="توضیح کوتاهی از خود بنویسید. در حد 200 کارکتر"
                   className="w-full"
@@ -81,22 +92,22 @@ function UserPanel() {
             <Button className="h-8 w-8 bg-mantle flex justify-center items-center rounded-md">
               <PlusSign />
             </Button>
-            <h3 className="bg-mantle p-2 w-full rounded-md bg-mantle h-9 text-xs">
+            <Text className="bg-mantle p-2 w-full rounded-md bg-mantle h-9 text-xs">
               زبان ها و تکنولوژی هایی که بلد هستید رو وارد کنید. (به زبان
               انگلیسی) :‌ مثال c programming language{" "}
-            </h3>
+            </Text>
           </div>
           <div className="flex items-center gap-x-2.5 bg-crust p-2 rounded-xl">
             <Button className="h-8 w-8 bg-mantle flex justify-center items-center rounded-md">
               <PlusSign />
             </Button>
-            <h3 className="bg-mantle p-2 w-full rounded-md bg-mantle h-9 text-xs">
+            <Text className="bg-mantle p-2 w-full rounded-md bg-mantle h-9 text-xs">
               لینک سوشال میدیا هایی که دارید رو اینجا بزارید.
-            </h3>
+            </Text>
           </div>
           <div className="flex items-center gap-x-2.5 bg-crust p-2 rounded-xl w-full">
             <form className="w-full">
-              <MyTextField
+              <TextInput
                 name="نظر در مورد سایت"
                 placeholder="نظرتون رو در مورد این سایت بنویسید. (بعد از ۲۰ روز در این قسمت امکان نوشتن هست) "
                 className="w-full"
@@ -111,48 +122,49 @@ function UserPanel() {
         </div>
 
         <h1 className="font-extrabold text-xl">تنظیمات</h1>
-        <MyCheckboxGroup className="bg-crust rounded-xl p-2 w-full flex flex-col gap-y-2.5 ">
-          <MyCheckbox value="sss">
-            آیا ۱۰ تا از آخرین نظراتتون در صفحه پروفایلتون توسط دیگران دیده شود؟
-          </MyCheckbox>
-          <MyCheckbox value="sss1">
-            آیا قسمت زبان هایی که بلدید در صفحه پروفایلتون توسط دیگران دیده شود؟
-          </MyCheckbox>
-          <MyCheckbox value="sss2">
-            آیا قسمت هایلایت ها و یادداشت های شیر شده توسط شما در قسمت
-            پروفایلتون قابل مشاهده باشد؟
-          </MyCheckbox>
-          <MyCheckbox value="sss3">
-            آیا قسمت سوشال مدیای شما در پروفایلتون قابل مشاهده توسط دیگران باشد؟
-          </MyCheckbox>
-          <MyCheckbox value="sss4">
-            آیا تصویر پروفایل شما برای دیگران قابل مشاهده باشد؟
-          </MyCheckbox>
-          <MyCheckbox value="ss5">
-            آیا امتیاز و جایگاه شما در چالش های هفتگی قابل مشاهده باشد؟
-          </MyCheckbox>
-          <MyCheckbox value="sss6">
-            آیا صفحه پروفایل برای شما ساخته شود؟
-          </MyCheckbox>
-        </MyCheckboxGroup>
+     <SettingCheckboxGroup className="bg-crust rounded-xl p-2 w-full flex flex-col gap-y-2.5">
+  <SettingCheckboxOption value="sss">
+    آیا ۱۰ تا از آخرین نظراتتون در صفحه پروفایلتون توسط دیگران دیده شود؟
+  </SettingCheckboxOption>
+  <SettingCheckboxOption value="sss1">
+    آیا قسمت زبان‌هایی که بلدید در صفحه پروفایلتون توسط دیگران دیده شود؟
+  </SettingCheckboxOption>
+  <SettingCheckboxOption value="sss2">
+    آیا قسمت هایلایت‌ها و یادداشت‌های شیر شده توسط شما در قسمت پروفایلتون قابل مشاهده باشد؟
+  </SettingCheckboxOption>
+  <SettingCheckboxOption value="sss3">
+    آیا قسمت سوشال مدیای شما در پروفایلتون قابل مشاهده توسط دیگران باشد؟
+  </SettingCheckboxOption>
+  <SettingCheckboxOption value="sss4">
+    آیا تصویر پروفایل شما برای دیگران قابل مشاهده باشد؟
+  </SettingCheckboxOption>
+  <SettingCheckboxOption value="ss5">
+    آیا امتیاز و جایگاه شما در چالش‌های هفتگی قابل مشاهده باشد؟
+  </SettingCheckboxOption>
+  <SettingCheckboxOption value="sss6">
+    آیا صفحه پروفایل برای شما ساخته شود؟
+  </SettingCheckboxOption>
+</SettingCheckboxGroup>
+
 
         <h1 className="font-extrabold text-xl">درخواست ها</h1>
         <div className="flex items-center gap-x-2.5 bg-crust p-2 rounded-xl">
-          <div className="bg-mantle p-2 w-full rounded-md h-9 text-xs">
+          <Text className="bg-mantle p-2 w-full rounded-md h-9 text-xs">
             شما یک کاربر عادی هستید برای تبدیل شدن به منتور درخواست بدید.{" "}
-          </div>
+          </Text>
+       
         </div>
         <h1 className="font-extrabold text-xl"> تنظیمات اولیه</h1>
         <form className="flex flex-col gap-2.5">
           <div className="grid grid-cols-4 gap-2.5 bg-crust p-2 rounded-xl">
             <div className="col-start-1 col-end-3">
-              <MyTextField name="نام کاربری" placeholder="نام کاربری" />
+              <TextInput name="نام کاربری" placeholder="نام کاربری" />
             </div>
             <div className="col-start-3 col-end-5">
-              <MyTextField name="رمز عبور" placeholder="رمز عبور" />
+              <TextInput name="رمز عبور" placeholder="رمز عبور" />
             </div>
             <div className="col-span-4">
-              <MyTextField name="ایمیل" placeholder="ایمیل" />
+              <TextInput name="ایمیل" placeholder="ایمیل" />
             </div>
             <div className="col-span-4">
               <Button className="bg-crust border border-red border-2 w-full p-2 h-9 text-red text-xs">
@@ -161,8 +173,8 @@ function UserPanel() {
             </div>
           </div>
 
-          <div className="grid grid-cols-9 gap-2.5 text-xs font-bold text-crust">
-            <div className="col-span-6">
+          <div className="grid grid-cols-11 gap-2.5 text-xs font-bold text-crust">
+            <div className="col-span-8">
               <Button className="bg-sapphire w-full p-2 h-9  ">ثبت</Button>
             </div>
             <div className="col-span-3">
