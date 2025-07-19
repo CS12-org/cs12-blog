c17c81dc0bccab280ba5a079665c3b2a6fbaa84a
mohammad <m.h.tazaroie753@gmail.com>
2025-07-19 17:15:18 +0430

    fix merge confilicts

--- app/routes.ts
+++ app/routes.ts
@@ -1,22 +1,15 @@
 import { type RouteConfig, layout, route } from "@react-router/dev/routes";
 
 export default [
-<<<<<<< Updated upstream
-=======
-<<<<<<< Updated upstream
   layout("layout/Main.tsx", [route("/", "routes/FeaturesPage.tsx")]),
-=======
->>>>>>> Stashed changes
-  layout("layout/Main.tsx", [route("/", "routes/Home.tsx")]),
+  // TODO : uncomment this later
+  // layout("layout/Main.tsx", [route("/", "routes/Home.tsx")]),
 
   route("/login", "routes/Login.tsx"),
   route("/signup", "routes/SignUp.tsx"),
-<<<<<<< Updated upstream
 
   route("/api/theme", "routes/api/theme.ts"),
-=======
   route("/forgot-password", "routes/forgotpassword.tsx"),
   route("/api/theme", "routes/api/theme.ts"),
->>>>>>> Stashed changes
->>>>>>> Stashed changes
+  
 ] satisfies RouteConfig;
