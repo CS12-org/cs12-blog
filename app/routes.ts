4337c0b420513f7702d8f25be11e5f19aa37f0a8
mohammad <m.h.tazaroie753@gmail.com>
2025-07-19 17:33:01 +0430

    fix forgot password functionality issues

--- app/routes.ts
+++ app/routes.ts
@@ -1,7 +1,7 @@
 import { type RouteConfig, layout, route } from "@react-router/dev/routes";
 
 export default [
-  layout("layout/Main.tsx", [route("/", "routes/FeaturesPage.tsx")]),
+  layout("layout/Main.tsx", [route("/", "routes/Home.tsx")]),
   // TODO : uncomment this later
   // layout("layout/Main.tsx", [route("/", "routes/Home.tsx")]),
 
@@ -9,7 +9,5 @@ export default [
   route("/signup", "routes/SignUp.tsx"),
 
   route("/api/theme", "routes/api/theme.ts"),
-  route("/forgot-password", "routes/forgotpassword.tsx"),
-  route("/api/theme", "routes/api/theme.ts"),
-  
+  route("/forgot-password", "routes/Forgotpassword.tsx"),
 ] satisfies RouteConfig;
