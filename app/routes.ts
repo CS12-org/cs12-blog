ac3e39087fec65f7e225210c5b8a0ee8a1b924e2
Fatima.Baghandeh <fatima.baghandeh@gmail.com>
2025-07-21 18:24:01 +0430

    feat: (wip) user panel page

--- app/routes.ts
+++ app/routes.ts
@@ -4,5 +4,11 @@ export default [
   layout("layout/Main.tsx", [
     route("/", "routes/FeaturesPage.tsx"),
     route("/*", "routes/NotFoundPage.tsx"),
+    route("/", "routes/UserPanel.tsx")
   ]),
+
+  route("/login", "routes/Login.tsx"),
+  route("/signup", "routes/SignUp.tsx"),
+  route("/api/theme", "routes/api/theme.ts"),
+ 
 ] satisfies RouteConfig;
