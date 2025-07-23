247f3d53598fe03f4e909e9feaab81fd8bd32697
unknown <amirali85mohebbi@gmail.com>
2025-07-23 13:56:22 +0330

    userpanel and usersavelist completion

--- app/routes/UserSavedpost.tsx
+++ app/routes/UserSavedpost.tsx
@@ -1,4 +1,4 @@
-import UserPanelList from "~/components/UserPanel/UserPanelList";
+import UserSaveList from "~/components/UserPanel/UserSaveList";
 import Profile from "../assets/images/user-profile.png";
 import PlusSign from "~/assets/images/plus-sign.svg?react";
 import ThemeSetting from "~/components/UserPanel/ThemeSetting";
@@ -18,7 +18,7 @@ function UserPanel() {
 
   return (
     <section className="flex gap-x-5 w-full items-start">
-      <UserPanelList
+      <UserSaveList
         mobileSidebarOpen={mobileSidebarOpen}
         onMobileSidebarChange={setMobileSidebarOpen}
       />
@@ -153,36 +153,38 @@ function UserPanel() {
       <span className="bg-base text-sapphire text-sm px-3 py-1 rounded-md font-Vazirmatn">برنامه نویسی</span>
       <span className="bg-base text-maroon text-sm px-3 py-1 rounded-md font-Vazirmatn">دفتلچه گیت</span>
     </div>
-    <button className=" w-[17px] h-[3px] flex items-center justify-center rounded hover:bg-mantle transition ">
-<svg
-  xmlns="http://www.w3.org/2000/svg"
-  className="w-5 h-5 text-subtext-0 hover:text-gray-800 cursor-pointer"
-  fill="currentColor"
-  viewBox="0 0 20 20"
->
-  <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zm6 0a2 2 0 11-4 0 2 2 0 014 0zm6 0a2 2 0 11-4 0 2 2 0 014 0z" />
-</svg>
-</button>
-<svg
-  xmlns="http://www.w3.org/2000/svg"
-  viewBox="0 0 28 28"
-  fill="currentColor"
-  className="w-15 h-5 text-green-400"
->
-  <g clipPath="url(#clip0_1162_6345)">
-    <path
-      fillRule="evenodd"
-      clipRule="evenodd"
-      d="M11.1518 4.31332C12.5477 2.3591 15.4521 2.3591 16.848 4.31332L17.0702 4.62443C17.3158 4.96836 17.7284 5.15252 18.1484 5.10586L19.1389 4.99579C21.3681 4.74811 23.2517 6.6317 23.004 8.8609L22.894 9.85143C22.8473 10.2715 23.0314 10.6839 23.3754 10.9296L23.6865 11.1518C25.6407 12.5477 25.6407 15.4521 23.6865 16.848L23.3754 17.0702C23.0314 17.3158 22.8473 17.7284 22.894 18.1484L23.004 19.1389C23.2517 21.3681 21.3681 23.2517 19.1389 23.004L18.1484 22.894C17.7284 22.8473 17.3158 23.0314 17.0702 23.3754L16.848 23.6865C15.4521 25.6407 12.5478 25.6407 11.1518 23.6865L10.9296 23.3754C10.6839 23.0314 10.2715 22.8473 9.85141 22.894L8.8609 23.004C6.6317 23.2517 4.74811 21.3681 4.99579 19.1389L5.10586 18.1484C5.15252 17.7284 4.96836 17.3158 4.62443 17.0702L4.31332 16.848C2.3591 15.4521 2.3591 12.5478 4.31332 11.1518L4.62443 10.9296C4.96836 10.6839 5.15252 10.2715 5.10586 9.85141L4.99579 8.8609C4.74811 6.6317 6.6317 4.74811 8.8609 4.99579L9.85143 5.10586C10.2715 5.15252 10.6839 4.96836 10.9296 4.62443L11.1518 4.31332ZM18.3249 10.8416C18.7805 11.2972 18.7805 12.036 18.3249 12.4915L13.8695 16.9469C13.2972 17.5193 12.3693 17.5193 11.797 16.9469L9.67495 14.8249C9.21935 14.3693 9.21935 13.6306 9.67495 13.175C10.1306 12.7194 10.8693 12.7194 11.3249 13.175L12.8333 14.6834L16.675 10.8416C17.1306 10.386 17.8693 10.386 18.3249 10.8416Z"
-      fill="#8BD5CA"
-    />
-  </g>
-  <defs>
-    <clipPath id="clip0_1162_6345">
-      <rect width="28" height="28" fill="white" />
-    </clipPath>
-  </defs>
-</svg>
+    <div className="flex items-center gap-2">
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 28 28"
+    fill="currentColor"
+    className="w-6 h-6 text-green-400"
+  >
+    <g clipPath="url(#clip0_1162_6345)">
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M11.1518 4.31332C12.5477 2.3591 15.4521 2.3591 16.848 4.31332L17.0702 4.62443C17.3158 4.96836 17.7284 5.15252 18.1484 5.10586L19.1389 4.99579C21.3681 4.74811 23.2517 6.6317 23.004 8.8609L22.894 9.85143C22.8473 10.2715 23.0314 10.6839 23.3754 10.9296L23.6865 11.1518C25.6407 12.5477 25.6407 15.4521 23.6865 16.848L23.3754 17.0702C23.0314 17.3158 22.8473 17.7284 22.894 18.1484L23.004 19.1389C23.2517 21.3681 21.3681 23.2517 19.1389 23.004L18.1484 22.894C17.7284 22.8473 17.3158 23.0314 17.0702 23.3754L16.848 23.6865C15.4521 25.6407 12.5478 25.6407 11.1518 23.6865L10.9296 23.3754C10.6839 23.0314 10.2715 22.8473 9.85141 22.894L8.8609 23.004C6.6317 23.2517 4.74811 21.3681 4.99579 19.1389L5.10586 18.1484C5.15252 17.7284 4.96836 17.3158 4.62443 17.0702L4.31332 16.848C2.3591 15.4521 2.3591 12.5478 4.31332 11.1518L4.62443 10.9296C4.96836 10.6839 5.15252 10.2715 5.10586 9.85141L4.99579 8.8609C4.74811 6.6317 6.6317 4.74811 8.8609 4.99579L9.85143 5.10586C10.2715 5.15252 10.6839 4.96836 10.9296 4.62443L11.1518 4.31332ZM18.3249 10.8416C18.7805 11.2972 18.7805 12.036 18.3249 12.4915L13.8695 16.9469C13.2972 17.5193 12.3693 17.5193 11.797 16.9469L9.67495 14.8249C9.21935 14.3693 9.21935 13.6306 9.67495 13.175C10.1306 12.7194 10.8693 12.7194 11.3249 13.175L12.8333 14.6834L16.675 10.8416C17.1306 10.386 17.8693 10.386 18.3249 10.8416Z"
+        fill="#8BD5CA"
+      />
+    </g>
+    <defs>
+      <clipPath id="clip0_1162_6345">
+        <rect width="28" height="28" fill="white" />
+      </clipPath>
+    </defs>
+  </svg>
+  <button className="w-6 h-6 flex items-center justify-center rounded hover:bg-mantle transition">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="w-5 h-5 text-subtext-0 hover:text-gray-800 cursor-pointer"
+      fill="currentColor"
+      viewBox="0 0 20 20"
+    >
+      <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zm6 0a2 2 0 11-4 0 2 2 0 014 0zm6 0a2 2 0 11-4 0 2 2 0 014 0z" />
+    </svg>
+  </button>
+</div>
   </div>
 
   
